35366620963251f64253eb1f45c114c79f8cf478
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2024-05-31 22:16:13 +0100

    Simplify flake (#35)

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     flake-utils,
     ...
@@ -17,8 +16,8 @@
       projectFile = "./CipherSuite/CipherSuite.fsproj";
       testProjectFile = "./CipherSuite.Test/CipherSuite.Test.fsproj";
       pname = "dotnet-cipher-suite";
-      dotnet-sdk = pkgs.dotnet-sdk_7;
-      dotnet-runtime = pkgs.dotnetCorePackages.runtime_7_0;
+      dotnet-sdk = pkgs.dotnet-sdk_8;
+      dotnet-runtime = pkgs.dotnetCorePackages.runtime_8_0;
       version = "0.1";
       dotnetTool = toolName: toolVersion: sha256:
         pkgs.stdenvNoCC.mkDerivation rec {
@@ -41,45 +40,17 @@
         };
     in {
       packages = {
-        fantomas = dotnetTool "fantomas" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fantomas.version "sha256-zYSF53RPbGEQt1ZBcHVYqEPHrFlmI1Ty3GQPW1uxPWw=";
-        fetchDeps = let
-          flags = [];
-          runtimeIds = ["win-x64"] ++ map (system: pkgs.dotnetCorePackages.systemToDotnetRid system) dotnet-sdk.meta.platforms;
-        in
-          pkgs.writeShellScriptBin "fetch-${pname}-deps" (builtins.readFile (pkgs.substituteAll {
-            src = ./nix/fetchDeps.sh;
-            pname = pname;
-            binPath = pkgs.lib.makeBinPath [pkgs.coreutils dotnet-sdk (pkgs.nuget-to-nix.override {inherit dotnet-sdk;})];
-            projectFiles = toString (pkgs.lib.toList projectFile);
-            testProjectFiles = toString (pkgs.lib.toList testProjectFile);
-            rids = pkgs.lib.concatStringsSep "\" \"" runtimeIds;
-            packages = dotnet-sdk.packages;
-            storeSrc = pkgs.srcOnly {
-              src = ./.;
-              pname = pname;
-              version = version;
-            };
-          }));
+        fantomas = dotnetTool "fantomas" (builtins.fromJSON (builtins.readFile ./.config/dotnet-tools.json)).tools.fantomas.version (builtins.head (builtins.filter (elem: elem.pname == "fantomas") ((import ./nix/deps.nix) {fetchNuGet = x: x;}))).sha256;
         default = pkgs.buildDotnetModule {
-          pname = pname;
+          inherit pname version projectFile testProjectFile dotnet-sdk dotnet-runtime;
           name = "cipher-suite";
-          version = version;
           src = ./.;
-          projectFile = projectFile;
           nugetDeps = ./nix/deps.nix;
           doCheck = true;
-          dotnet-sdk = dotnet-sdk;
-          dotnet-runtime = dotnet-runtime;
         };
       };
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (with dotnetCorePackages;
-            combinePackages [
-              dotnet-sdk_7
-              dotnetPackages.Nuget
-            ])
-        ];
+        buildInputs = [dotnet-sdk];
         packages = [
           pkgs.alejandra
           pkgs.nodePackages.markdown-link-check
